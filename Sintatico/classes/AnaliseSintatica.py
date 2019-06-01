8dd5808ef014fe909684704bd97a638bca5c8a2c
Heuller Soares Vilela Silva <heullergit@gmail.com>
2019-06-01 15:52:19 -0300

    mais algumas funcoes feitas

--- Sintatico/classes/AnaliseSintatica.py
+++ Sintatico/classes/AnaliseSintatica.py
@@ -36,83 +36,132 @@ class AnaliseSintatica:
             tudoCerto = True
         else:
             tudoCerto = False
-            print('erro sintatico declaracao')
+            print('erro sintatico oficial declaracao')
+            return tudoCerto
+
+        print('declaracao tudo certo')
         return tudoCerto
 
     def var_declaracao(self):
         tudoCerto = True
-        contador = 0
-        while(tudoCerto and contador < 1): #enquanto não foi encotrado erro e será executado apenas uma vez até o final
-            if(not self.tipo_especificador()):
-                print('erro sintatico var_declaracao 1')
+        if(not self.tipo_especificador()):
+            print('erro sintatico var_declaracao 1')
+            tudoCerto = False
+            return tudoCerto
+        print('var_declaracao correto 1')
+        if(not self.ident()):
+            print('erro sintatico var_declaracao 2')
+            tudoCerto = False
+            return tudoCerto
+        print('var_declaracao correto 2')
+        if(proximo() == ';'):
+            self.match(';')
+            print('var_declaracao correto 3')
+        elif(self.abre_colchete()):
+            if(not self.num_int()):
+                print('erro sintatico var_declaracao 3')
                 tudoCerto = False
-                break
-            if(not self.ident()):
-                print('erro sintatico var_declaracao 2')
+                return tudoCerto
+            if(not self.fecha_colchete()):
+                print('erro sintatico var_declaracao 4')
                 tudoCerto = False
-                break
-            if(proximo() == ';'):
-                self.match(';')
-            elif(self.abre_colchete()):
-                if(not self.num_int()):
-                    print('erro sintatico var_declaracao 3')
-                    tudoCerto = False
-                    break
-                if(not self.fecha_colchete()):
-                    print('erro sintatico var_declaracao 4')
-                    tudoCerto = False
-                    break
-                if(not self.match(';')):
-                    print('erro sintatico var_declaracao 5')
-                    tudoCerto = False
-                    break
-            else:
-                print('erro sintatico oficial var_declaracao')
+                return tudoCerto
+            if(not self.match(';')):
+                print('erro sintatico var_declaracao 5')
                 tudoCerto = False
-                break
-            contador += 1
+                return tudoCerto
+        else:
+            print('erro sintatico oficial var_declaracao')
+            tudoCerto = False
+            return tudoCerto
         
+        print('var_declaracao tudo certo ')
         return tudoCerto
-    
-    def abre_colchete(self):
-        return self.match('[')
 
     def tipo_especificador(self):
+        tudoCerto = True
         if(self.proximo() == 'int'):
             self.match('int')
+            print('tipo_especificador correto 1')
         elif(self.proximo() == 'float'):
             self.match('float')
+            print('tipo_especificador correto 2')
         elif(self.proximo() == 'void'):
             self.match('void')
+            print('tipo_especificador correto 3')
         elif(self.proximo() == 'struct):
             self.match('struct')
-            self.ident()
-            self.abre_chave()
-            self.atributos_declaracao()
-            self.fecha_chave()
+            print('tipo_especificador struct')
+            if(not self.ident()):
+                print('erro sintatico tipo_especificador 1')
+                tudoCerto = False
+                return tudoCerto
+            if(not self.abre_chave()):
+                print('erro sintatico tipo_especificador 2')
+                tudoCerto = False
+                return tudoCerto
+            if(not self.atributos_declaracao()):
+                print('erro sintatico tipo_especificador 3')
+                tudoCerto = False
+                return tudoCerto
+            if(not self.fecha_chave()):
+                print('erro sintatico tipo_especificador 4')
+                tudoCerto = False
+                return tudoCerto
         else:
-            print('erro sintatico tipo_especificador')
-            return False
+            print('erro sintatico oficial tipo_especificador')
+            tudoCerto = False
+            return tudoCerto
         
-        print('tipo_especificador')
-        return False
+        return tudoCerto
 
     def atributos_declaracao(self):
-        self.var_declaracao()
+        return self.var_declaracao()
 
     def fun_declaracao(self):
-        self.tipo_especificador()
-        self.ident()
-        self.match('(')
-        self.params()
-        self.match(')')
-        self.composto_decl()
+        tudoCerto = True
+        if(not self.tipo_especificador()):
+            tudoCerto = False
+            print('erro sintatico fun_declaracao 1')
+            return tudoCerto
+        if(not self.ident()):
+            tudoCerto = False
+            print('erro sintatico fun_declaracao 2')
+            return tudoCerto
+        if(not self.match('(')):
+            tudoCerto = False
+            print('erro sintatico fun_declaracao 3')
+            return tudoCerto
+        if(not self.params()):
+            tudoCerto = False
+            print('erro sintatico fun_declaracao 4')
+            return tudoCerto
+        if(not self.match(')')):
+            tudoCerto = False
+            print('erro sintatico fun_declaracao 5')
+            return tudoCerto
+        if(not self.composto_decl()):
+            tudoCerto = False
+            print('erro sintatico fun_declaracao 6')
+            return tudoCerto
+        
+        print('fun_declaracao tudo certo')
+        return tudoCerto
 
     def params(self):
         if(self.proximo() == 'void'):
             self.match('void')
+            print('params correto 1')
+            tudoCerto = True
+            return tudoCerto
+        elif(self.param_lista()):
+            print('params correto 2')
+            tudoCerto = True
+            return tudoCerto
         else:
-            self.param_lista()
+            print('erro sintatico oficial params')
+            tudoCerto = False
+            return tudoCerto
 
     def param_lista(self):
         self.param()
@@ -123,17 +172,7 @@ class AnaliseSintatica:
         self.tipo_especificador()
         self.ident()
 
-    #def analisar(self):
-'''
-    def chamada(self):
-        print(self.segundaChamada())
-        print('chamada')
+    def abre_colchete(self):
+        return self.match('[')
 
-    def segundaChamada(self):
-        return self.terceiraChamada()
-        print('segunda chamada')
-    
-    def terceiraChamada(self):
-        print('terceira chamada')
-        return True
-'''
+    #def analisar(self):
