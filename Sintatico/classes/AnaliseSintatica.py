83ae07f19569cf760ec69fac982fbfda70eaef17
Heuller Soares Vilela Silva <heullergit@gmail.com>
2019-06-01 22:07:12 -0300

    ate a linha 23

--- Sintatico/classes/AnaliseSintatica.py
+++ Sintatico/classes/AnaliseSintatica.py
@@ -164,13 +164,254 @@ class AnaliseSintatica:
             return tudoCerto
 
     def param_lista(self):
-        self.param()
+        if(self.param()):
+            tudoCerto = True
+            print('param_lista correto 1')
+        else:
+            tudoCerto = False
+            return tudoCerto
         while(self.proximo() == ','):
-            self.param()
+            self.match(',')
+            print('param_lista correto 2')
+            if(not self.param()):
+                tudoCerto = False
+                return tudoCerto
+        return tudoCerto
 
     def param(self):
-        self.tipo_especificador()
-        self.ident()
+        tudoCerto = True
+        if(not self.tipo_especificador()):
+            tudoCerto = False
+            print('erro sintatico param 1')
+            return tudoCerto
+        if(not self.ident()):
+            tudoCerto = False
+            print('erro sintatico param 2')
+            return tudoCerto
+        if(self.abre_colchete()):
+            print('tudo certo param 1')
+            if(not self.fecha_colchete()):
+                tudoCerto = False
+                print('erro sintatico param 3')
+                return tudoCerto
+        else:
+            print('implementar aqui')
+
+        return tudoCerto
+
+    def composto_decl(self):
+        tudoCerto = True
+        if(not self.abre_chave()):
+            tudoCerto = False
+            print('erro sintatico composto_decl 1')
+            return tudoCerto
+        if(not self.local_declaracoes()):
+            tudoCerto = False
+            print('erro sintatico composto_decl 2')
+            return tudoCerto
+        if(not self.comando_lista()):
+            tudoCerto = False
+            print('erro sintatico composto_decl 3')
+            return tudoCerto
+        if(not self.fecha_chave()):
+            tudoCerto = False
+            print('erro sintatico composto_decl 4')
+            return tudoCerto
+
+        return tudoCerto
+
+    def local_declaracoes(self): #verificar com calma aqui depois
+        tudoCerto = False
+        while(self.var_declaracao()):
+            print('tudo certo local_declaracoes 1')
+            tudoCerto = True
+        return tudoCerto
+
+    def comando_lista(self): #verificar com calma aqui depois
+        tudoCerto = False
+        while(self.comando()):
+            print('tudo certo local_declaracoes 1')
+            tudoCerto = True
+        return tudoCerto
+
+    def comando(self):
+        if(self.expressao_decl()):
+            print('tudo certo comando 1')
+            tudoCerto = True
+        elif(self.composto_decl()):
+            print('tudo certo comando 2')
+            tudoCerto = True
+        elif(self.selecao_decl()):
+            print('tudo certo comando 3')
+            tudoCerto = True
+        elif(self.iteracao_decl()):
+            print('tudo certo comando 4')
+            tudoCerto = True
+        elif(self.retorno_decl()):
+            print('tudo certo comando 5')
+            tudoCerto = True
+        else:
+            print('erro sintatico oficial comando')
+            tudoCerto = False
+        
+        return tudoCerto
+
+    def expressao_decl(self):
+        if(self.proximo() == ';'):
+            self.match(';')
+            tudoCerto = True
+            print('tudo certo expressao_decl 1')
+        elif(self.expressao()):
+            tudoCerto = self.match(';')
+            print('tudo certo expressao_decl 2')
+        else:
+            tudoCerto = False
+            print('erro sintatico oficial expressao_decl')
+
+        return tudoCerto
+
+    def selecao_decl(self):
+        tudoCerto = True
+        if(not self.match('if')):
+            tudoCerto = False
+            print('erro sintatico selecao_decl 1')
+            return tudoCerto
+        if(not self.match('(')):
+            print('erro sintatico selecao_decl 2')
+            tudoCerto = False
+            return tudoCerto
+        if(not self.expressao()):
+            print('erro sintatico selecao_decl 3')
+            tudoCerto = False
+            return tudoCerto
+        if(not self.match(')')):
+            print('erro sintatico selecao_decl 4')
+            tudoCerto = False
+            return tudoCerto
+        if(not self.comando()):
+            print('erro sintatico selecao_decl 5')
+            tudoCerto = False
+            return tudoCerto
+        if(self.proximo() == 'else'):
+            self.match('else')
+            print('tudo certo selecao_decl 1')
+            if(not self.comando()):
+                tudoCerto = False
+                print('erro sintatico selecao_decl 6')
+                return tudoCerto
+        else:
+            print('implementar aqui')
+
+    def iteracao_decl(self):
+        tudoCerto = True
+        if(not self.match('while')):
+            tudoCerto = False
+            print('erro sintatico iteracao_decl 1')
+            return tudoCerto
+        if(not self.match('(')):
+            tudoCerto = False
+            print('erro sintatico iteracao_decl 2')
+            return tudoCerto
+        if(not self.expressao()):
+            tudoCerto = False
+            print('erro sintatico iteracao_decl 3')
+            return tudoCerto
+        if(not self.match(')')):
+            tudoCerto = False
+            print('erro sintatico iteracao_decl 4')
+            return tudoCerto
+        if(not self.comando()):
+            tudoCerto = False
+            print('erro sintatico iteracao_decl 5')
+            return tudoCerto
+        return tudoCerto
+
+    def retorno_decl(self):
+        tudoCerto = True
+        if(not self.match('return')):
+            print('erro sintatico retorno_decl 1')
+            tudoCerto = False
+            return tudoCerto
+        if(self.proximo() == ';'):
+            self.match(';')
+            print('tudo certo retorno_decl 1')
+            tudoCerto = True
+            return tudoCerto
+        elif(self.expressao()):
+            if(not self.match(';')):
+                print('erro sintatico retorno_decl 2')
+                tudoCerto = False
+                return tudoCerto
+        else:
+            print('erro sintatico oficial retorno_decl')
+            tudoCerto = False
+            return tudoCerto
+
+        return tudoCerto
+
+    def expressao(self):
+        tudoCerto = True
+        if(self.var()):
+            print('tudo certo expressao 1')
+            if(not self.match('=')):
+                tudoCerto = False
+                print('erro sintatico expressao 1')
+                return tudoCerto
+            if(not self.expressao()):
+                tudoCerto = False
+                print('erro sintatico expressao 2')
+                return tudoCerto
+        elif(self.expressao_simples()):
+            print('tudo certo expressao 2')
+            tudoCerto = True
+            return tudoCerto
+        else:
+            print('erro sintatico oficial expressao')
+            tudoCerto = False
+            return tudoCerto
+
+    return tudoCerto
+
+    def var(self):
+        tudoCerto = True
+        if(self.ident()):
+            print('tudo certo var 1')
+            if(not self.abre_colchete()):
+                print('erro sintatico var 1')
+                tudoCerto = False
+                return tudoCerto
+            if(not self.expressao()):
+                tudoCerto = False
+                print('erro sintatico var 2')
+                return tudoCerto
+            if(not self.fecha_colchete()):
+                print('erro sintatico var 3')
+                tudoCerto = False
+                return tudoCerto
+        else:
+            print('erro sintatico oficial var')
+            tudoCerto = False
+        
+        return tudoCerto
+
+    def expressao_simples(self):
+        tudoCerto = True
+        if(self.expressao_soma()):
+            print('tudo certo expressao_simples')
+            if(not self.relacional()):
+                print('erro sintatico expressao_simples 1')
+                tudoCerto = False
+                return tudoCerto
+            if(not self.expressao_soma()):
+                print('erro sintatico expressao_simples 2')
+                tudoCerto = False
+                return tudoCerto
+        else:
+            tudoCerto = False
+            print('erro sintatico oficial expressao_simples')
+            return tudoCerto
+        return tudoCerto
+
 
     def abre_colchete(self):
         return self.match('[')
